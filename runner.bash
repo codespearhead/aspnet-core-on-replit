7ffc009adef510da8edab7ce73772ca865a2eb3e
codespearhead <72931357+codespearhead@users.noreply.github.com>
2023-06-28 14:48:51 +0000

    [UPDATE]: make code comments more descriptive

--- runner.bash
+++ runner.bash
@@ -1,14 +1,15 @@
-# Check if any visible directory exists in working directory
+# Check if any visible directory exists in the working directory
 VISIBLE_DIR=$(find . -type d -not -path '*/\.*' | wc -l)
 if [[ $VISIBLE_DIR -eq 1 ]]
 then
+	# Create a solution if there isn't
   read -p 'Project name: ' NAME
   echo $NAME > project_name.replit
   dotnet new webapp -o $NAME --no-https
 	dotnet new gitignore
 	echo "$NAME was created"
 else
-  # Retrieve the name of the solution
+  # Retrieve its name and run it if there is
   NAME=$(head -n 1 project_name.replit)
 	dotnet watch --project $NAME run --urls=http://0.0.0.0:8080
 fi
