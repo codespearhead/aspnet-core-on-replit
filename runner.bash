64fdbdee206f9527f6e0ba51903e73067189eb0e
Pedro Aguiar <72931357+codespearhead@users.noreply.github.com>
2024-03-07 22:31:33 -0400

    [REFACTOR]: fix runner.bash indentation

--- runner.bash
+++ runner.bash
@@ -2,14 +2,14 @@
 VISIBLE_DIR=$(find . -type d -not -path '*/\.*' | wc -l)
 if [[ $VISIBLE_DIR -eq 1 ]]
 then
-	# Create a solution if there isn't
-  read -p 'Project name: ' NAME
-  echo $NAME > project_name.replit
-  dotnet new webapp -o $NAME --no-https
-	dotnet new gitignore
-	echo "$NAME was created"
+    # Create a solution if there isn't
+    read -p 'Project name: ' NAME
+    echo $NAME > project_name.replit
+    dotnet new webapp -o $NAME --no-https
+    dotnet new gitignore
+    echo "$NAME was created"
 else
-  # Retrieve its name and run it if there is
-  NAME=$(head -n 1 project_name.replit)
-	dotnet watch --project $NAME run --urls=http://0.0.0.0:8080
+    # Retrieve its name and run it if there is
+    NAME=$(head -n 1 project_name.replit)
+    dotnet watch --project $NAME run --urls=http://0.0.0.0:8080
 fi
