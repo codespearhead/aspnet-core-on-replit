37cdec523b1b623880413063f728ce66d844bd46
Pedro Aguiar <72931357+codespearhead@users.noreply.github.com>
2024-03-07 22:57:49 -0400

    [REFACTOR]: be more eloquent in variable names

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ VISIBLE_DIR=$(find . -type d -not -path '*/\.*' | wc -l)
 # If only one project directory exists:
 if [[ $VISIBLE_DIR -eq 1 ]]
 then
-    read -p 'Project name: ' NAME
-    echo $NAME > project_name.replit
-    dotnet new webapp -o $NAME --no-https
+    read -p 'Project name: ' PROJECT_NAME
+    echo $PROJECT_NAME > project_name.replit
+    dotnet new webapp -o $PROJECT_NAME --no-https
     dotnet new gitignore
-    echo "$NAME was created"
+    echo "$PROJECT_NAME was created"
 else
     # Retrieve project name from file (assuming the first line)
-    NAME=$(head -n 1 project_name.replit)
-    dotnet watch --project $NAME run --urls=http://0.0.0.0:8080
+    PROJECT_NAME=$(head -n 1 project_name.replit)
+    dotnet watch --project $PROJECT_NAME run --urls=http://0.0.0.0:8080
 fi
